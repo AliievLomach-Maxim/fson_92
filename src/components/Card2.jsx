558099766916d515f318ecb288a802334133f2d2
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-11-21 21:45:23 +0200

    add less

---
+++ src/components/Card2.jsx
@@ -0,0 +1,12 @@
+import data from '../data.json'
+
+const Card2 = () => {
+	return data.map((el, ind) => (
+		<div className='container' key={el.id}>
+			<h1>{el.title}</h1>
+			<h5>{el.body}</h5>
+		</div>
+	))
+}
+
+export default Card2
