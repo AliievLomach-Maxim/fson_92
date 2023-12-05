9f3beb6171047c7a9940afcfedae34a71362cb0d
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-12-05 21:39:52 +0200

    add less

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,32 @@
-const Header = ({ showModal }) => {
-	console.log(10 < 12 && 'component')
-	return (
-		<nav className='navbar bg-dark mb-3'>
-			<div className='container-fluid'>
-				<span className='navbar-brand mb-0 h1 text-success'>Navbar</span>
-				<button className='btn btn-outline-success' onClick={showModal}>
-					Open Modal
-				</button>
-			</div>
-		</nav>
-	)
+import { Component, PureComponent } from 'react'
+
+class Header extends PureComponent {
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	return nextProps.showModal !== this.props.showModal ? true : false
+	// 	// if (nextProps.showModal !== this.props.showModal) return true
+	// 	// if (nextProps.name !== this.props.showModal) return true
+	// 	// if (nextProps.info !== this.props.showModal) return true
+	// 	// if (nextProps.todo !== this.props.showModal) return true
+	// 	// if (nextProps.address !== this.props.address) return true
+	// 	// if (nextProps.address.street !== this.props.address.street) return true
+	// 	// if (nextProps.showModal !== this.props.showModal) return true
+	// 	// return false
+	// }
+
+	render() {
+		console.log('render Header')
+		const { showModal } = this.props
+		return (
+			<nav className='navbar bg-dark mb-3'>
+				<div className='container-fluid'>
+					<span className='navbar-brand mb-0 h1 text-success'>Navbar</span>
+					<button className='btn btn-outline-success' onClick={showModal}>
+						Open Modal
+					</button>
+				</div>
+			</nav>
+		)
+	}
 }
 
 export default Header
-
-// && || ??
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,39 @@
-const Modal = ({ children, hideModal }) => {
-	return (
-		<div
-			className='modal fade show'
-			style={{ display: 'block', backdropFilter: 'blur(5px)' }}
-		>
-			<div className='modal-dialog'>
-				<div className='modal-content'>
-					<div className='modal-header'>
-						<h5 className='modal-title'> Modal</h5>
-						<button
-							type='button'
-							className='btn-close'
-							aria-label='Close'
-							onClick={hideModal}
-						></button>
+import { Component } from 'react'
+
+class Modal extends Component {
+	handleEsc = (e) => {
+		if (e.code === 'Escape') this.props.hideModal()
+		console.log('Press')
+	}
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleEsc)
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleEsc)
+	}
+
+	render() {
+		const { children, hideModal } = this.props
+		return (
+			<div className='modal fade show' style={{ display: 'block', backdropFilter: 'blur(5px)' }}>
+				<div className='modal-dialog'>
+					<div className='modal-content'>
+						<div className='modal-header'>
+							<h5 className='modal-title'> Modal</h5>
+							<button
+								type='button'
+								className='btn-close'
+								aria-label='Close'
+								onClick={hideModal}
+							></button>
+						</div>
+						<div className='modal-body'>{children}</div>
 					</div>
-					<div className='modal-body'>{children}</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Modal
